89538c1bb4d1c4386b504d8c5a4f15137b02b3a9
duuaaaa <duajaved0327@gmail.com>
2024-11-30 10:54:12 +0500

    Add files via upload

---
+++ mylogin.php
@@ -0,0 +1,55 @@
+<?php
+include("dbconnection.php");
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label>username</label>
+        <input type="text" name="username" required><br><br>
+        <label>password</label>
+        <input type="password" name="password" required id="showpass"><br><br>
+        <input type="checkbox" onclick= "showpassword()">
+        <label>show password</label>
+
+        <input type="submit" value="login" name="loginbtn">
+    </form>
+
+    <?php
+    if(isset($_POST['loginbtn'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $q = "SELECT * FROM Login WHERE username ='$username' and password ='$password'";
+        $run = mysqli_query($conn,$q);
+        $total_rows=mysqli_num_rows($run);
+        // echo $total_rows;
+        if($total_rows==1) {
+            $_SESSION['username']=$username;
+            $_SESSION['last_time']=time();
+           header('location:dashboard.php');
+        }else{
+            "<script>alert('user or password is incorrect')</script>";
+        }
+    }
+    ?>
+
+    <script>
+        function showpassword(){
+            let a= document.getElementById("showpass");
+            if(a.type == "password"){
+                a.type="text";
+            }else{
+                a.type="password";
+            }
+
+        }
+        </script>
+</body>
+</html>
